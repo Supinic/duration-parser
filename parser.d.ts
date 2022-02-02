91245fd26d53512b3be365d8d8f1444f2a1997a0
Supinic <supinic@protonmail.com>
2022-02-02 14:02:12 +0100

    added types

---
+++ parser.d.ts
@@ -0,0 +1,34 @@
+declare type Time = number;
+
+export declare type Range = {
+    string: string;
+    time: number;
+    start: number;
+    end: number;
+};
+export declare type Result = {
+    time: Time;
+    ranges: Array<Range>;
+};
+export declare type Unit = {
+    name: string;
+    aliases: Array<string>;
+    value: number;
+};
+
+export declare interface CommonOptions {
+    target?: Unit["name"];
+    returnData?: boolean;
+    ignoreError?: boolean;
+}
+
+declare interface ResultOptions extends CommonOptions {
+    returnData: true;
+}
+declare interface TimeOptions extends CommonOptions {
+    returnData: false | undefined;
+}
+
+export declare function parse (input: string, options: ResultOptions): Result;
+export declare function parse (input: string, options: TimeOptions): Time;
+export declare function parse (input: string): Time;
